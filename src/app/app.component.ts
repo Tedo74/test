5523ac39241b9de23e3f3fc40686c576827c7f5d
Konstantin Stoimenov <konstantin.stoimenov@pliant.io>
2023-10-13 14:34:14 +0300

    Create project.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,36 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {FormControl, FormGroup, UntypedFormControl} from "@angular/forms";
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit{
   title = 'testing';
+  formControlValue: boolean|null = false;
+
+  form = new FormGroup({
+    control: new FormControl(false)
+  });
+
+  ngOnInit(): void {
+    this.form.get('control')?.valueChanges.subscribe({
+      next: (value)=> {
+        console.log('value changed: ', value);
+        this.formControlValue = value;
+      }
+    });
+
+    const ctrl = this.form.get('control') as UntypedFormControl;
+    setTimeout(()=> {
+      ctrl.setValue(true);
+    }, 4000);
+    setTimeout(()=> {
+      ctrl.setValue(false, {emitEvent: false});
+    }, 6000);
+    setTimeout(()=> {
+      this.form.get('control')?.setValue(true);
+    }, 8000);
+  }
 }
